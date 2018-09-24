95e39b6e0d82a8eb76d33f2437459f6f2fc93cf4
Bitmapped <bpowell@bitmapped.net>
2018-09-24 12:46:09 -0400

    Finish tests

--- src/OfficeFileProperties.Tests/FileAccessors/Dao/AccdbFileTests.cs
+++ src/OfficeFileProperties.Tests/FileAccessors/Dao/AccdbFileTests.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DocumentFormat.OpenXml;
 
 namespace OfficeFileProperties.FileAccessors.Dao.Tests
 {
@@ -16,7 +16,7 @@ namespace OfficeFileProperties.FileAccessors.Dao.Tests
         [TestMethod()]
         public void AccdbGetAuthorTest()
         {
-            var file = new OfficeFile(@"..\..\SampleFiles\Test.Accdb");
+            var file = new OfficeFile(@"..\..\SampleFiles\Test.accdb");
             file.OpenFile();
 
             Assert.AreEqual("Test Author", file.Author);
@@ -25,9 +25,24 @@ namespace OfficeFileProperties.FileAccessors.Dao.Tests
         }
 
         [TestMethod()]
+        public void AccdbSetAuthorTest()
+        {
+            var file = new OfficeFile(@"..\..\SampleFiles\WriteTest.accdb");
+            var testValue = $"Test Author {DateTime.Now}";
+
+            file.OpenFile(true);
+            file.Author = testValue;
+            file.CloseFile();
+
+            file.OpenFile();
+            Assert.AreEqual(testValue, file.Author);
+            file.CloseFile();
+        }
+
+        [TestMethod()]
         public void AccdbGetCompanyTest()
         {
-            var file = new OfficeFile(@"..\..\SampleFiles\Test.Accdb");
+            var file = new OfficeFile(@"..\..\SampleFiles\Test.accdb");
             file.OpenFile();
 
             Assert.AreEqual("Test Company", file.Company);
@@ -36,9 +51,50 @@ namespace OfficeFileProperties.FileAccessors.Dao.Tests
         }
 
         [TestMethod()]
+        public void AccdbSetCompanyTest()
+        {
+            var file = new OfficeFile(@"..\..\SampleFiles\WriteTest.accdb");
+            var testValue = $"Test Company {DateTime.Now}";
+
+            file.OpenFile(true);
+            file.Company = testValue;
+            file.CloseFile();
+
+            file.OpenFile();
+            Assert.AreEqual(testValue, file.Company);
+            file.CloseFile();
+        }
+
+        [TestMethod()]
+        public void AccdbGetTitleTest()
+        {
+            var file = new OfficeFile(@"..\..\SampleFiles\Test.accdb");
+            file.OpenFile();
+
+            Assert.AreEqual("Test Title", file.Title);
+
+            file.CloseFile();
+        }
+
+        [TestMethod()]
+        public void AccdbSetTitleTest()
+        {
+            var file = new OfficeFile(@"..\..\SampleFiles\WriteTest.accdb");
+            var testValue = $"Test Title {DateTime.Now}";
+
+            file.OpenFile(true);
+            file.Title = testValue;
+            file.CloseFile();
+
+            file.OpenFile();
+            Assert.AreEqual(testValue, file.Title);
+            file.CloseFile();
+        }
+
+        [TestMethod()]
         public void AccdbGetCommentsTest()
         {
-            var file = new OfficeFile(@"..\..\SampleFiles\Test.Accdb");
+            var file = new OfficeFile(@"..\..\SampleFiles\Test.accdb");
             file.OpenFile();
 
             Assert.AreEqual("Test Comments", file.Comments);
@@ -47,9 +103,24 @@ namespace OfficeFileProperties.FileAccessors.Dao.Tests
         }
 
         [TestMethod()]
+        public void AccdbSetCommentsTest()
+        {
+            var file = new OfficeFile(@"..\..\SampleFiles\WriteTest.accdb");
+            var testValue = $"Test Comments {DateTime.Now}";
+
+            file.OpenFile(true);
+            file.Comments = testValue;
+            file.CloseFile();
+
+            file.OpenFile();
+            Assert.AreEqual(testValue, file.Comments);
+            file.CloseFile();
+        }
+
+        [TestMethod()]
         public void AccdbGetCreatedTimeUtcTest()
         {
-            var file = new OfficeFile(@"..\..\SampleFiles\Test.Accdb");
+            var file = new OfficeFile(@"..\..\SampleFiles\Test.accdb");
             file.OpenFile();
 
             Assert.AreEqual(new DateTime(2016, 3, 1, 15, 24, 25, DateTimeKind.Utc), file.CreatedTimeUtc);
@@ -57,23 +128,11 @@ namespace OfficeFileProperties.FileAccessors.Dao.Tests
             file.CloseFile();
         }
 
-        [TestMethod]
-        public void AccdbGetCustomPropertiesTest()
-        {
-            var file = new OfficeFile(@"..\..\SampleFiles\Test.Accdb");
-            file.OpenFile();
-
-            var expectedValue = new Dictionary<string, object>() { { "Test1", "Test" }, { "Test2", 1 } };
-
-            CollectionAssert.AreEqual((ICollection)expectedValue, (ICollection)file.CustomProperties);
-
-            file.CloseFile();
-        }
 
         [TestMethod()]
         public void AccdbGetModifiedTimeUtcTest()
         {
-            var file = new OfficeFile(@"..\..\SampleFiles\Test.Accdb");
+            var file = new OfficeFile(@"..\..\SampleFiles\Test.accdb");
             file.OpenFile();
 
             Assert.AreEqual(new DateTime(2018, 9, 21, 16, 02, 33, DateTimeKind.Utc), file.ModifiedTimeUtc);
@@ -84,7 +143,7 @@ namespace OfficeFileProperties.FileAccessors.Dao.Tests
         [TestMethod()]
         public void AccdbOpenAndCloseFileTest()
         {
-            var file = new OfficeFile(@"..\..\SampleFiles\Test.Accdb");
+            var file = new OfficeFile(@"..\..\SampleFiles\Test.accdb");
             file.OpenFile();
             file.CloseFile();
         }
--- src/OfficeFileProperties.Tests/FileAccessors/Dao/MdbFileTests.cs
+++ src/OfficeFileProperties.Tests/FileAccessors/Dao/MdbFileTests.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DocumentFormat.OpenXml;
 
 namespace OfficeFileProperties.FileAccessors.Dao.Tests
 {
@@ -16,7 +16,7 @@ namespace OfficeFileProperties.FileAccessors.Dao.Tests
         [TestMethod()]
         public void MdbGetAuthorTest()
         {
-            var file = new OfficeFile(@"..\..\SampleFiles\Test.Mdb");
+            var file = new OfficeFile(@"..\..\SampleFiles\Test.mdb");
             file.OpenFile();
 
             Assert.AreEqual("Test Author", file.Author);
@@ -25,9 +25,24 @@ namespace OfficeFileProperties.FileAccessors.Dao.Tests
         }
 
         [TestMethod()]
+        public void MdbSetAuthorTest()
+        {
+            var file = new OfficeFile(@"..\..\SampleFiles\WriteTest.mdb");
+            var testValue = $"Test Author {DateTime.Now}";
+
+            file.OpenFile(true);
+            file.Author = testValue;
+            file.CloseFile();
+
+            file.OpenFile();
+            Assert.AreEqual(testValue, file.Author);
+            file.CloseFile();
+        }
+
+        [TestMethod()]
         public void MdbGetCompanyTest()
         {
-            var file = new OfficeFile(@"..\..\SampleFiles\Test.Mdb");
+            var file = new OfficeFile(@"..\..\SampleFiles\Test.mdb");
             file.OpenFile();
 
             Assert.AreEqual("Test Company", file.Company);
@@ -36,9 +51,50 @@ namespace OfficeFileProperties.FileAccessors.Dao.Tests
         }
 
         [TestMethod()]
+        public void MdbSetCompanyTest()
+        {
+            var file = new OfficeFile(@"..\..\SampleFiles\WriteTest.mdb");
+            var testValue = $"Test Company {DateTime.Now}";
+
+            file.OpenFile(true);
+            file.Company = testValue;
+            file.CloseFile();
+
+            file.OpenFile();
+            Assert.AreEqual(testValue, file.Company);
+            file.CloseFile();
+        }
+
+        [TestMethod()]
+        public void MdbGetTitleTest()
+        {
+            var file = new OfficeFile(@"..\..\SampleFiles\Test.mdb");
+            file.OpenFile();
+
+            Assert.AreEqual("Test Title", file.Title);
+
+            file.CloseFile();
+        }
+
+        [TestMethod()]
+        public void MdbSetTitleTest()
+        {
+            var file = new OfficeFile(@"..\..\SampleFiles\WriteTest.mdb");
+            var testValue = $"Test Title {DateTime.Now}";
+
+            file.OpenFile(true);
+            file.Title = testValue;
+            file.CloseFile();
+
+            file.OpenFile();
+            Assert.AreEqual(testValue, file.Title);
+            file.CloseFile();
+        }
+
+        [TestMethod()]
         public void MdbGetCommentsTest()
         {
-            var file = new OfficeFile(@"..\..\SampleFiles\Test.Mdb");
+            var file = new OfficeFile(@"..\..\SampleFiles\Test.mdb");
             file.OpenFile();
 
             Assert.AreEqual("Test Comments", file.Comments);
@@ -47,9 +103,24 @@ namespace OfficeFileProperties.FileAccessors.Dao.Tests
         }
 
         [TestMethod()]
+        public void MdbSetCommentsTest()
+        {
+            var file = new OfficeFile(@"..\..\SampleFiles\WriteTest.mdb");
+            var testValue = $"Test Comments {DateTime.Now}";
+
+            file.OpenFile(true);
+            file.Comments = testValue;
+            file.CloseFile();
+
+            file.OpenFile();
+            Assert.AreEqual(testValue, file.Comments);
+            file.CloseFile();
+        }
+
+        [TestMethod()]
         public void MdbGetCreatedTimeUtcTest()
         {
-            var file = new OfficeFile(@"..\..\SampleFiles\Test.Mdb");
+            var file = new OfficeFile(@"..\..\SampleFiles\Test.mdb");
             file.OpenFile();
 
             Assert.AreEqual(new DateTime(2016, 3, 1, 15, 24, 25, DateTimeKind.Utc), file.CreatedTimeUtc);
@@ -57,23 +128,11 @@ namespace OfficeFileProperties.FileAccessors.Dao.Tests
             file.CloseFile();
         }
 
-        [TestMethod]
-        public void MdbGetCustomPropertiesTest()
-        {
-            var file = new OfficeFile(@"..\..\SampleFiles\Test.mdb");
-            file.OpenFile();
-
-            var expectedValue = new Dictionary<string, object>() { { "Test1", "Test" }, { "Test2", 1 } };
-
-            CollectionAssert.AreEqual((ICollection)expectedValue, (ICollection)file.CustomProperties);
-
-            file.CloseFile();
-        }
 
         [TestMethod()]
         public void MdbGetModifiedTimeUtcTest()
         {
-            var file = new OfficeFile(@"..\..\SampleFiles\Test.Mdb");
+            var file = new OfficeFile(@"..\..\SampleFiles\Test.mdb");
             file.OpenFile();
 
             Assert.AreEqual(new DateTime(2018, 9, 21, 15, 14, 35, DateTimeKind.Utc), file.ModifiedTimeUtc);
@@ -84,7 +143,7 @@ namespace OfficeFileProperties.FileAccessors.Dao.Tests
         [TestMethod()]
         public void MdbOpenAndCloseFileTest()
         {
-            var file = new OfficeFile(@"..\..\SampleFiles\Test.Mdb");
+            var file = new OfficeFile(@"..\..\SampleFiles\Test.mdb");
             file.OpenFile();
             file.CloseFile();
         }
